04996c223a581b25c659ac4638311455666419bd
bhupendrakhatri2002 <bhupinilgar717@gmail.com>
2024-05-02 18:56:14 +0530

    create userschema And connect to  database

---
+++ models/userschema.js
@@ -0,0 +1,35 @@
+const mongoose =require("mongoose")
+
+const userSchema = new mongoose.Schema(
+    {
+    name:{
+        type:String,
+        trim:true,
+        required:[true,"name is required"],
+        minlength:[4,"name must be atleast 4 character long"]
+    },
+    username:{
+        type:String,
+        trim:true,
+        unique:true,
+        required:[true,"username is required"],
+    minlength:[4,"username must be atleast 4 character long"]
+    },
+    email:{
+        type:String,
+        trim:true,
+        lowercase:true,
+        required:[true,"name is required"],
+        match: [
+            /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+            "Please fill a valid email address",
+        ],
+    },
+
+    password:String,
+},
+{ timestamps: true }
+);
+
+const User =mongoose.model("user",userSchema);
+module.exports=User;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var router = express.Router();
 
+const User =require('../models/userschema')
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get("/about",function(req,res,next){
+  res.render("about",{})
+})
+router.get("/login",function(req,res,next){
+  res.render("login",{})
+})
+router.get("/register",function(req,res,next){
+  res.render("register",{})
+})
+router.post("/register-user",async function(req,res,next){
+  try {
+    const newUser = new User(req.body);
+    await newUser.save();
+    res.send(newUser)
+    // res.redirect("/login");
+} catch (error) {
+    res.send(error);
+}
+});
+
+router.get("/about", function (req, res, next) {
+res.render("about");
+})
 
 module.exports = router;
