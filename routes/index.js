aa46cc358648be6e64d6ebfd00c6da6b4895a3d7
bhupendrakhatri2002 <bhupinilgar717@gmail.com>
2024-05-08 18:56:53 +0530

    adding authenticate part and profile in code

--- routes/index.js
+++ routes/index.js
@@ -7,23 +7,19 @@ const passport=require("passport");
 const LocalStrategy =require("passport-local");
 
 
-passport.use(new LocalStrategy(User.authenticate));
+passport.use(new LocalStrategy(User.authenticate()));
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
 
-router.get("/about",function(req,res,next){
-  res.render("about",{})
-})
-router.get("/login",function(req,res,next){
-  res.render("login",{})
-})
+
 router.get("/register",function(req,res,next){
   res.render("register",{})
 })
+
 router.post("/register-user",async function(req,res,next){
   try {
     // only for save data----
@@ -31,20 +27,54 @@ router.post("/register-user",async function(req,res,next){
     // await newUser.save();
 
     // for passport use and for register a user-
-    const{username,email,name ,password}=req.body
-    await User.register({name,username,email},password)
+    const{username,email,name ,password}=req.body;
+    await User.register({name,username,email},password);
     res.redirect("/login");
 } catch (error) {
     res.send(error);
 }
 });
 
-router.get("/about", function (req, res, next) {
-res.render("about");
-})
 
-router.get("/profile", function (req, res, next) {
+router.get("/login",function(req,res,next){
+  res.render("login",{})
+})
+// login router---------------
+router.post("/login-user",
+    passport.authenticate("local", 
+    {
+       successRedirect:"/profile",
+       failureRedirect:"/login",
+    }),
+     function(req,res,next){}
+)
+
+router.get("/about",function(req,res,next){
+  res.render("about",{})
+});
+
+router.get("/profile",isLoggedIn, function (req, res, next) {
   res.render("profile");
+  });
+
+router.get("/logout-user",function(req,res,next){
+  req.logout(()=>{
+    res.redirect("/login");
   })
+ 
+})  
+
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  else{
+    res.redirect("/login");
+  }
+ 
+}
+
+
+
 
 module.exports = router;
